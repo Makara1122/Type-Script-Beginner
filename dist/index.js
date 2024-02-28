0babae354b53e5a92ece609a9ea28df2e0768b81
Your Name <you@example.com>
2024-02-28 10:13:04 +0700

    Type Script Beginner

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let a = 20;
+let b = "Hello Type Script";
+console.log(`a value : ${a}`);
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+"use strict";
+
+let a: number = 20;
+let b : string = "Hello Type Script"
+console.log(`a value : ${a}`)
+
+// hello world
