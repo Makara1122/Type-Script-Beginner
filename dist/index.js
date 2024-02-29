7e41fe83e366c8e61f224cde71f55a13c53ab90e
Your Name <you@example.com>
2024-02-29 09:28:20 +0700

    Type Script Medium

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,41 @@
 "use strict";
 let a = 20;
 let b = "Hello Type Script";
+let isPublished;
+let x;
 console.log(`a value : ${a}`);
+let arrays = [1, 2, 3, 4, 5];
+let studentNames = ["sok kha", "bona", "data"];
+let numberOne;
+arrays.map(num => console.log(num++));
+let employees;
+employees = [
+    [1, "Ra"],
+    [2, "Raj"]
+];
+console.log(employees[0][0]);
+var Sizes;
+(function (Sizes) {
+    Sizes[Sizes["Small"] = 30] = "Small";
+    Sizes[Sizes["Medium"] = 31] = "Medium";
+    Sizes[Sizes["Large"] = 32] = "Large";
+})(Sizes || (Sizes = {}));
+var SizeString;
+(function (SizeString) {
+    SizeString["Small"] = "small";
+    SizeString["Medium"] = "medium";
+    SizeString["Large"] = "large";
+})(SizeString || (SizeString = {}));
+var PizzaPromotion;
+(function (PizzaPromotion) {
+    PizzaPromotion["Monday"] = "Buy 1 free 1";
+    PizzaPromotion["Tuesday"] = "normal";
+    PizzaPromotion["Wednesday"] = "buy 3 free 1";
+})(PizzaPromotion || (PizzaPromotion = {}));
+console.log(PizzaPromotion.Monday.toLowerCase());
+let calculateTax = function (amount, tax) {
+    let total = amount - (tax * amount);
+    return total;
+};
+let taxCalculated = calculateTax(100, 200);
+console.log(taxCalculated);
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,36 @@
 
 let a: number = 20;
 let b : string = "Hello Type Script"
+let isPublished : true
+let x : any
 console.log(`a value : ${a}`)
 
 // hello world
+
+let arrays : number[] = [1,2,3,4,5]
+let studentNames : string[] = ["sok kha", "bona", "data"]
+let numberOne : number
+arrays.map(num => console.log(num++))
+
+let employees : [number, string][]
+
+employees = [
+    [1,"Ra"],
+    [2,"Raj"]
+]
+console.log(employees[0][0])
+    
+enum Sizes {Small = 30 , Medium , Large}
+enum SizeString {Small = "small", Medium = "medium", Large = "large"}
+
+enum PizzaPromotion {Monday = "Buy 1 free 1", Tuesday = "normal", Wednesday = "buy 3 free 1"}
+console.log(PizzaPromotion.Monday.toLowerCase())
+
+//  return function with TypeScript
+
+let calculateTax = function (amount : number, tax : number) : number {
+    let total = amount - (tax * amount );
+    return total;
+}
+let taxCalculated = calculateTax(100,200);
+console.log(taxCalculated)
